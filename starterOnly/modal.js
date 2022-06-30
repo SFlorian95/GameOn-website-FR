19ad8cac06590a0a6fa19e61e07ef5cee2488462
SFlorian95 <subramanyam.florian@outlook.fr>
2022-06-30 12:03:48 +0200

    implémentation de la fermeture de la modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -14,10 +14,15 @@ const formData = document.querySelectorAll(".formData");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+document.querySelector('.close').addEventListener("click", closeModal);
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
 
